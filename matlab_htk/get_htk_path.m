0653a18624f07283e7cfb77f41c7254813921df5
Ron Weiss <ronweiss@gmail.com>
2010-01-06 14:13:28 -0500

    isolated hmm from original monolithic ronw-matlab-tools repository

--- matlab_htk/get_htk_path.m
+++
@@ -1,7 +0,0 @@
-function htkpath = get_htk_path()
-% htkpath = get_htk_path()
-%
-% Gives a centralized place to set the path to HTK binaries
-
-% on labrosa machines:
-htkpath = '~drspeech/opt/htk/bin.linux/';
